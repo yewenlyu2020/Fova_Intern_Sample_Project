6f523720cd9a4d4a6694f0f6a77e9f28a1d18381
yewenlyu2019 <46354696+yewenlyu2019@users.noreply.github.com>
2020-06-04 06:14:22 -0700

    Merge files: SageMaker only allows a single script

--- model/code/inference.py
+++ model/code/inference.py
@@ -4,10 +4,120 @@ import os
 import requests
 
 import torch
+import torch.nn as nn
 from PIL import Image
 from torchvision import transforms
 
-from mobilenetv2_cifar10 import MobileNetV2
+# https://github.com/huyvnphan/PyTorch_CIFAR10
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride,
+                      padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU6(inplace=True)
+        )
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(round(inp * expand_ratio))
+        self.use_res_connect = self.stride == 1 and inp == oup
+
+        layers = []
+        if expand_ratio != 1:
+            # pw
+            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
+        layers.extend([
+            # dw
+            ConvBNReLU(hidden_dim, hidden_dim,
+                       stride=stride, groups=hidden_dim),
+            # pw-linear
+            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(oup),
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=10, width_mult=1.0):
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        input_channel = 32
+        last_channel = 1280
+
+        # CIFAR10
+        inverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 1],  # Stride 2 -> 1 for CIFAR-10
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+        # END
+
+        # building first layer
+        input_channel = int(input_channel * width_mult)
+        self.last_channel = int(last_channel * max(1.0, width_mult))
+
+        # CIFAR10: stride 2 -> 1
+        features = [ConvBNReLU(3, input_channel, stride=1)]
+        # END
+
+        # building inverted residual blocks
+        for t, c, n, s in inverted_residual_setting:
+            output_channel = int(c * width_mult)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features.append(
+                    block(input_channel, output_channel, stride, expand_ratio=t))
+                input_channel = output_channel
+        # building last several layers
+        features.append(ConvBNReLU(
+            input_channel, self.last_channel, kernel_size=1))
+        # make it nn.Sequential
+        self.features = nn.Sequential(*features)
+
+        # building classifier
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(self.last_channel, num_classes),
+        )
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.mean([2, 3])
+        x = self.classifier(x)
+        return x
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -35,7 +145,7 @@ def model_fn(model_dir):
 
     # logger.info('Current device: {}'.format(device))
 
-    with open(os.path.join(model_dir, 'mobilenet_v2.pt'), 'rb') as f:
+    with open(os.path.join(model_dir, 'model.pt'), 'rb') as f:
         model.load_state_dict(torch.load(f))
     model.to(device).eval()
 
@@ -74,10 +184,11 @@ def input_fn(request_body, request_content_type):
         transforms.Resize(size=256),
         transforms.CenterCrop(size=224),
         transforms.ToTensor(),
-        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        transforms.Normalize([0.4914, 0.4822, 0.4465],
+                             [0.2023, 0.1994, 0.2010])
     ])
 
-    return image_transform(image_data) 
+    return image_transform(image_data)
 
 
 def predict_fn(input_data, model):
@@ -117,13 +228,15 @@ def output_fn(prediction, response_content_type='application/json'):
         (str): a json formatted string from the prediction tensor object
     """
     logger.info('Serializing the output...')
-    classes = {0:'plane', 1:'car', 2:'bird', 3:'cat', 4:'deer', 5:'dog', 6:'frog', 7:'horse', 8:'ship', 9:'truck'}
+    classes = {0: 'plane', 1: 'car', 2: 'bird', 3: 'cat', 4: 'deer',
+               5: 'dog', 6: 'frog', 7: 'horse', 8: 'ship', 9: 'truck'}
 
     topk, topclass = prediction.topk(3, dim=1)
     result = []
 
     for i in range(3):
-        pred = {'prediction': classes[topclass.cpu().numpy()[0][i]], 'score': f'{topk.cpu().numpy()[0][i] * 100}%'}
+        pred = {'prediction': classes[topclass.cpu().numpy(
+        )[0][i]], 'score': f'{topk.cpu().numpy()[0][i] * 100}%'}
         logger.info('Adding prediction: %s', pred)
         result.append(pred)
 
